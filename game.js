27ea4f790d90328ba751d1b001602d13ee3b318a
MdDubey <md@whatever.com>
2014-10-21 15:20:34 +0530

    changing to MVC

--- game.js
+++ game.js
@@ -1,27 +1,57 @@
-var game = {};
+var game = {
+	observer: ''
+};
 
-// var shapes = require('./shapes.json');
-
-// console.log(shapes);
-
-game.Point = function(x,y){
+game.Point = function(x, y) {
 	this.x = x;
 	this.y = y;
 };
 
-game.Node = function(x,y){
-	this.point = new game.Point(x,y);
+game.Node = function(x, y) {
+	this.point = new game.Point(x, y);
 	this.edges = [];
 };
 
-game.Line = function (startNode, endNode) {
+game.Line = function(startNode, endNode) {
 	this.startNode = startNode;
 	this.endNode = endNode;
-	this.color = rgb(0,0,0);
+	this.visited = false;
 };
 
-game.drawShape = function(container,lines){
+game.drawShape = function(container, lines) {
 
+};
+
+game.startGame = function(observer) {
+	var shapeData = {
+		"height": 400,
+		"width": 400,
+		"nodesData": [
+			[20, 20],
+			[320, 20],
+			[20, 320],
+			[320, 320]
+		],
+		"linesData": [
+			[20, 20, 320, 20],
+			[20, 320, 320, 320],
+			[20, 20, 20, 320],
+			[320, 20, 320, 320]
+		]
+	};
+	game.observer = observer;
+	var shape = {};
+	shape.height = shapeData.height;
+	shape.width = shapeData.width;
+	shape.nodes = shapeData.nodesData.map(function(nodeData){
+		return new game.Node(nodeData[0],nodeData[1]);
+	});
+	shape.lines = shapeData.linesData.map(function(lineData) {
+		var start = new game.Node(lineData[0], lineData[1]);
+		var end = new game.Node(lineData[2], lineData[3]);
+		return new game.Line(start, end);
+	});
+	game.observer.onGameStart(shape);
 };
 
 // exports.game = game;
--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 // var game = require('./game').game;
 
-var init = function() {
-	var shape = {
-		"height":400,"width":400,
-		"nodes":[[20,20],[320,20],[20,320],[320,320]],
-		"lines":[[20,20,320,20],[20,320,320,320],[20,20,20,320],[320,20,320,320]]
-	};
+var observer = {};
+
+observer.onGameStart = function(shape){
 	var container = $('div');
 	var svgHTML = '<svg width="'+shape.width+'" height="'+shape.height+'">';
 	shape.lines.forEach(function(line){
 		var lineHTML = '<line class="edge" x1="X1" y1="Y1" x2="X2" y2="Y2" style="stroke:rgb(0,0,0);stroke-width:5" />\n';
-		lineHTML = lineHTML.replace('X1',line[0]).replace('Y1',line[1]).replace('X2',line[2]).replace('Y2',line[3]);
+		lineHTML = lineHTML.replace('X1',line.startNode.point.x).replace('Y1',line.startNode.point.y).replace('X2',line.endNode.point.x).replace('Y2',line.endNode.point.y);
 		svgHTML += lineHTML;
 	});
 	shape.nodes.forEach(function(node){
 		var circleHTML = '<circle class="node" cx="CX" cy="CY" r="10" stroke="black" stroke-width="3" fill="skyblue"/>\n';
-		circleHTML = circleHTML.replace('CX',node[0]).replace('CY',node[1]);
+		circleHTML = circleHTML.replace('CX',node.point.x).replace('CY',node.point.y);
 		svgHTML += circleHTML;
 	});
 	svgHTML += '</svg>';
 	container.html(svgHTML);
+}
+
+var init = function() {
+	game.startGame(observer);
 };
 
 $(document).ready(init);
